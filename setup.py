cd97228f990d2359edb6cc024777515fb9a0c1fb
Ebuka Hills <flyg101@gmail.com>
2017-09-22 22:15:32 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='kudisms',
+    packages=['kudisms'],
+    version='0.1',
+    description='A Python Library for Kudi SMS HTTP API',
+    author='Ebuka Hills',
+    author_email='flyg101@gmail.com',
+    url='https://github.com/flyg101/kudisms-py',
+    download_url='https://github.com/flyg101/kudisms-py/archive/0.1.tar.gz',
+    keywords=['sms', 'api', 'kudi', 'python'],
+    classifiers=[],
+)
